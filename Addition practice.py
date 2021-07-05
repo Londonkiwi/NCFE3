023b8d25f5cb16625a16f6c9e7897370451d0964
Londonkiwi <85025111+Londonkiwi@users.noreply.github.com>
2021-07-05 05:04:52 +0100

    Add files via upload

---
+++ Addition practice.py
@@ -0,0 +1,18 @@
+# Assignment of addends to variables
+a=3.5;b=8.2;c=4.7
+# Display summation of variables
+print ("The sum of 3.5, 8.2 and 4.7 is",a+b+c)
+# Prompt user to input their first number
+num=input ("Enter your first number:")
+# Assignment of first input to float "a"
+a=float(num)
+# Prompt user to input their second number
+num=input ("Enter your second number:")
+# Assignment of second input to float "b"
+b=float(num)
+# Prompt user to input their third number
+num=input ("Enter your third number number:")
+# Assignment of third input to float "c"
+c=float(num)
+# Display summation of numbers to user
+print ("The sum of number 1, number 2 and number 3 is",a+b+c)
---
+++ Test.py
@@ -0,0 +1,6 @@
+a=78;b=56;c=10
+sum=a+b+c
+print (sum)
+input=num("Pick a number")
+print=sum+num
+
