8132e96369b353f5fd772bb6f0f8701610123f67
Londonkiwi <85025111+Londonkiwi@users.noreply.github.com>
2021-07-05 07:11:42 +0100

    Add files via upload

---
+++ CODING EXERCISE 1 - Kilo to pound conversion.py
@@ -0,0 +1,7 @@
+# Kilogram to pound conversion program
+# Prompt input of kilograms from user
+num=input ("Enter the kilogram amount you wish to convert into pounds:")
+# Assignment of kilogram amount to float "k"
+k=float(num)
+# Conversion of kgs to lbs and output to user
+print ("The pound conversion of the kilogram amount entered is",k*2.205)
---
+++ CODING EXERCISE 2 - Calculator - Elliot.py
@@ -0,0 +1,24 @@
+def div_op(numer,denom):
+
+
+
+
+ if denom == 0:
+
+
+
+
+    print("Error - You cannot divide by 0. Please choose an appropriate denominator")
+
+
+
+
+ else:
+
+    
+
+    div_res=(numer/denom);
+
+    
+
+    print("{0}/{1} = {2}".format(numer,denom,div_res))
